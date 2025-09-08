1873933e1c0a670cf86a64b7c14753ae38a35452
Ademilua Adeola Israel <142322376+AdemiluaAdeola@users.noreply.github.com>
2025-09-08 08:20:46 +0100

    settings.py updated

--- mysite/settings.py
+++ mysite/settings.py
@@ -164,3 +164,17 @@ CSRF_TRUSTED_ORIGINS = [
     'https://*.onrender.com',
     'https://*.yourdomain.com',
 ]
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
