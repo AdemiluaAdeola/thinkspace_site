01916ef73c9b51335bbf8b95bc74ea05f7cec18e
Ademilua Adeola Israel <142322376+AdemiluaAdeola@users.noreply.github.com>
2025-09-08 06:54:07 +0100

    update wsgi.py

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -10,8 +10,12 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
+from pathlib import Path
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), 'staticfiles'))
+application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
